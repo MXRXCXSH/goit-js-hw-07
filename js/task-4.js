61ec0d45d3626553bebdebe5a4962c1a647ba4dc
Andrii Khomenko <akhomenko1995@gmail.com>
2025-12-06 20:23:34 +0100

    tasks -3, -4, -5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
-
+const form = document.querySelector('.login-form');
+form.addEventListener('submit', submitLoginForm);
+function submitLoginForm(event) {
+  event.preventDefault();
+  const emailInput = form.elements['email'];
+  const passwordInput = form.elements['password'];
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+  if (emailValue === '' || passwordValue === '') {
+    return alert('All form fields must be filled in');
+  }
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+  console.log(formData);
+  form.reset();
+}
